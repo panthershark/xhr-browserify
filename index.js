5a50a18590bde9bee71707b2f7bd3edd0eb89b0d
Dan Hough <daniel.hough@gmail.com>
2014-02-18 21:23:39 +0100

    Made it possible to make requests that do not expect json. Probably not the best way but a good start.

--- index.js
+++ index.js
@@ -47,15 +47,27 @@ var xhr = function(uri, done) {
     });
 
     res.on('end', function () {
-      done(JSON.parse(buf));
+      done(buf);
     });
+  });
+};
+
+var json = function(uri, done) {
+  xhr(uri, function (buf) {
+    done(JSON.parse(buf));
   });
 };
 
 module.exports = function(uri, options, callback) {
   if (typeof(options) == 'function') {
     callback = options;
-    options = {};
+    options = {
+      json: true
+    };
+  } else if (typeof(options) == 'object') {
+    if (typeof(options.json) == 'undefined') {
+      options.json = true;
+    }
   }
 
   if (typeof(uri) === 'string') {
@@ -64,6 +76,9 @@ module.exports = function(uri, options, callback) {
 
   if (options.jsonp) {
     jsonp(uri, callback);
+  }
+  if (options.json) {
+    json(uri, callback);
   }
   else {
     xhr(uri, callback);
